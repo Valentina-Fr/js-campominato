2c3e7a471c7ac94d943fe00d9646e904da9d527c
Valentina-Fr <v_frasca@virgilio.it>
2021-07-14 09:35:01 +0200

    Add arrays

---
+++ js/script.js
@@ -0,0 +1,19 @@
+//STEP
+// aggiungo array bombe vuoto
+//2 aggiungo array numeri utente vuoto
+//3 funzione per randomizzare numero da 1 a 100
+//4 invoco la funzione 16 volte e inserisco numeri nell'array
+
+//5 chiedere al giocatare numero da 1 a 10
+//6 verificare se è presente nell'array bombe, se si termina partita
+//7 verificare se è presente nell'array utetente ed aggiungere. Se è presente richiedere numero.
+//8 continuare a chiedere per 100 - 16 volte
+
+//9 a termine partita stampare messaggio e punteggio
+
+
+//Array bombe
+let bombArray = [];
+
+//Array numeri scelti dall'utente
+let userArray = [];
