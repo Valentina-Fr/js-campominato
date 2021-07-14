dfd492e3c49858fc931d84bebd1b2062f89ba7ee
Valentina-Fr <v_frasca@virgilio.it>
2021-07-14 10:51:11 +0200

    Add user choice

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 //STEP
-// aggiungo array bombe vuoto
+//1 aggiungo array bombe vuoto
 //2 aggiungo array numeri utente vuoto
 //3 funzione per randomizzare numero da 1 a 100
 //4 invoco la funzione 16 volte e inserisco numeri nell'array
 
-//5 chiedere al giocatare numero da 1 a 10
+//5 chiedere al giocatare numero da 1 a 100
 //6 verificare se è presente nell'array bombe, se si termina partita
 //7 verificare se è presente nell'array utetente ed aggiungere. Se è presente richiedere numero.
 //8 continuare a chiedere per 100 - 16 volte
@@ -18,14 +18,35 @@ let bombArray = [];
 //Array numeri scelti dall'utente
 let userArray = [];
 
+const max = 20;
+const min = 1;
+
 //Randomizzare 16 numeri
 while (bombArray.length < 16) {
-    let random = bombRandom(100, 1);
+    let random = bombRandom(max, min);
     if (!isListed(random, bombArray)) {
         insert(random, bombArray);
     }
 }
 console.table(bombArray);
+
+//Scelta numeri utente
+let userNumber;
+
+while (max - bombArray.length !== userArray.length && !isListed(userNumber, bombArray)) {
+    do {userNumber = parseInt(prompt("Scegli un numero da " + min + " a " + max))}
+    while (isNaN(userNumber) || userNumber < min || userNumber > max);
+    if(isListed(userNumber, bombArray)) {
+        alert ("Hai perso, il tuo totale punti è: " + userArray.length);
+    } else if (!isListed(userNumber, userArray)) {
+        insert(userNumber, userArray); 
+    } 
+    if (max - bombArray.length === userArray.length) {
+        alert ("Hai vinto, il tuo totale punti è " + userArray.length);
+    } 
+}
+
+console.table(userArray); 
 
 //Funzione che randomizza numero 
 function bombRandom (numMax, numMin) {
@@ -40,6 +61,6 @@ function isListed(num, numArray){
 }
 
 //Funzione che inserisce elemento in un array
- function insert (num, numArray) {
-     return numArray.push(num);   
- }
+function insert (num, numArray) {
+    return numArray.push(num);   
+}
